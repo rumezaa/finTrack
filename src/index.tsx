6e0af211a7b7b6af307a5f04d3fa01b9f1bd0a35
Astroneeor <nerroralam@gmail.com>
2025-02-15 17:57:15 -0700

    deelted index

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";  // Import your App component
-
-// Render your React app into the root div of your popup HTML
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
-);
