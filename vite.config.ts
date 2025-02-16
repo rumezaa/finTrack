1562344d7109199e904080764d56b42e1868579b
Neeor Alam <nerroralam@gmail.com>
2025-02-15 20:35:42 -0700

    Revert "Script injection"

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: "main.html",   // React UI popup
-        content: "src/content.ts" // Content script (NO .tsx)
-      },
-      output: {
-        format: "iife"
-      }
-    }
-  }
-});
+})
